e5abf2ed53f6d2ab246885fa708d43f56f495a3d
Eli Citron <ebcitron@gmail.com>
2019-04-15 16:17:29 -0500

    Things are working

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -17,23 +17,28 @@ class Counter extends Component {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
+
+        console.log('this.props', this.props);
+        console.log('this.state', this.state);
+       
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button 
+                onClick={e => {
+                    this.props.increment();
+                    }}
+                    >
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button
+                 onClick={e => {
+                     this.props.decrement();
+                     }}
+                     >
                     -
                 </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
+ 
             </p>
         );
     }
